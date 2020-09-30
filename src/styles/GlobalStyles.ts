98b55fd9d312b76c47276a0842d80daf0f0e8b5c
AlienDev66 <p.capitango65@gmail.com>
2020-10-01 00:03:44 +0100

    :recycle: Refatorando cores.

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -12,8 +12,9 @@ html, body, #root{
  background: var(--color-background)
 }
 :root{
- --color-header: #320F1C;
+ --color-button: #320F1C;
  --color-text1: #001e36;
  --color-background: #F6F6F6;
+ --color-header: #FFF3E2;
 }
 `;
