b7e7a732a7cb0ff8292f6d2c6b5bedd59a9c6208
AlienDev66 <p.capitango65@gmail.com>
2020-09-30 23:09:08 +0100

    :rocket: Deploy.

---
+++ src/components/HeaderHome/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const HeaderHome: React.FC = () => {
+  return (
+    <Container>
+      <h1>HeaderHomeToTheFace</h1>
+    </Container>
+  );
+};
+
+export default HeaderHome;
