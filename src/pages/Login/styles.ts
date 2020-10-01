4ee4637c05774f5e05f62c72b617595d8e9a5270
AlienDev66 <p.capitango65@gmail.com>
2020-10-01 22:14:28 +0100

    :sparkles: Criando estilização para o login.

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,5 +1,27 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background: var(--color-header);
+`;
 
+export const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 460px;
+  height: 550px;
+  background: var(--color-login);
+  border-radius: 10px;
+  box-shadow: 10px;
+`;
+
+export const ImageLogin = styled.img`
+  margin-top: 10px;
+  width: 69px;
+  height: 67px;
 `;
