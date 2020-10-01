00d6dc6050635d79e31db5e9a65fd3ae639f6c32
Domingos Capitango <59874434+AlienDev66@users.noreply.github.com>
2020-10-01 09:22:17 +0100

    :sparkles: Criando rotas para Login, Register.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
+import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const Routes = () => {
-  return(
+  return (
     <BrowserRouter>
-      <Route component={Home} path="/" exact/>
+      <Route component={Home} path="/" exact />
+      <Route component={Login} path="/login" />
+      <Route component={Register} path="/register" />
     </BrowserRouter>
   );
-}
+};
 
 export default Routes;
